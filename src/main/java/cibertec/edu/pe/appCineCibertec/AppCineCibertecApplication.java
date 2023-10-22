6e4631b19e7afc14e86cf4d7894269ddb53d378a
maria309isabel <isa.mar.xv@gmail.com>
2023-10-22 09:47:18 -0500

    semana9

---
+++ src/main/java/cibertec/edu/pe/appCineCibertec/AppCineCibertecApplication.java
@@ -0,0 +1,13 @@
+package cibertec.edu.pe.appCineCibertec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppCineCibertecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppCineCibertecApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cibertec/edu/pe/appCineCibertec/AppCineCibertecApplicationTests.java
@@ -0,0 +1,13 @@
+package cibertec.edu.pe.appCineCibertec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppCineCibertecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
